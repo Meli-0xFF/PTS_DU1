ab9d0a39bab0637156f48664712828a69fc7202f
Meli-0xFF <martin.melichercik@gmail.com>
2020-03-07 21:58:28 +0100

    Return values of functions fixed, adding new functions.
    Signed-off-by: Meli-0xFF <martin.melichercik@gmail.com>

--- relation.py
+++ relation.py
@@ -1,6 +1,10 @@
 class Relation:
-    M = set()
-    R = set()
+    def __init__(self):
+        self.R=set()
+        self.M=set()
+
+    def __iter__(self):
+        return self.R.__iter__()
 
     def contains(self, *elements):
         return tuple(elements) in self.R
@@ -12,10 +16,35 @@ class Relation:
         self.R.remove(tuple(elements))
 
     def union(self, R2):
-        return self.R.union(R2)
+        self.R.union(R2.R)
 
     def intersection(self, R2):
-        return self.R.intersection(R2)
+        self.R.intersection(R2.R)
 
     def subtraction(self, R2):
-        return self.R.difference(R2) 
+        self.R.difference(R2.R)
+
+    def inverse(self):
+        self.R = set(map(lambda t : t[::-1], self.R))
+ 
+    def composition(self, R2):
+        res = set()
+        for t1 in self.R:
+            for t2 in R2.R:
+                if t1[1] == t2[0]:
+                    res.add((t1[0], t2[1]))
+        self.R = set(res)
+
+    def is_reflexive(self):
+        return len(self.M) == len(filter(lambda m : True if (m, m) in self.R else False, self.M))
+
+    def is_symetric(self):
+        return 0 == len(filter(lambda t : True if (t[1], t[0]) not in self.R else False, self.R))
+
+    def is_transitive(self):
+        for t1 in self.R:
+            for t2 in self.R:
+                if t1[1] == t2[0]:
+                    if (t1[0], t2[1]) not in self.R:
+                        return False
+        return True
