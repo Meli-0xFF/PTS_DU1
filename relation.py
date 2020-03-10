f5dc8b97e37e30c83a9b6f370bf2c78b2aea1de8
Meli-0xFF <martin.melichercik@gmail.com>
2020-03-10 02:25:09 +0100

    Making class immutable, refactoring functions.
    Signed-off-by: Meli-0xFF <martin.melichercik@gmail.com>

--- relation.py
+++ relation.py
@@ -1,47 +1,54 @@
 class Relation:
-    def __init__(self):
-        self.R=set()
-        self.M=set()
+    def __init__(self, M, R=set()):
+        self.R = R
+        self.M = M
 
     def __iter__(self):
         return self.R.__iter__()
 
-    def contains(self, *elements):
-        return tuple(elements) in self.R
+    def __next__(self):
+        return self.R.__next__()
 
-    def add(self, *elements):
-        self.R.add(tuple(elements))
+    def contains(self, x, y):
+        return (x,y) in self.R
 
-    def remove(self, *elements):
-        self.R.remove(tuple(elements))
+    def add(self, x, y):
+        return Relation(self.M, R=self.R | {(x,y)})
+
+    def remove(self, x, y):
+        return Relation(self.M, R=self.R - {(x,y)})
 
     def union(self, R2):
-        self.R.union(R2.R)
+        return Relation(self.M, R=self.R | R2.R)
 
     def intersection(self, R2):
-        self.R.intersection(R2.R)
+        return Relation (self.M, R=self.R & R2.R)
 
     def subtraction(self, R2):
-        self.R.difference(R2.R)
+        return Relation(self.M, R=self.R - R2.R)
 
     def inverse(self):
-        self.R = set(map(lambda t : t[::-1], self.R))
+        return Relation(self.M, R=set(map(lambda t : t[::-1], self.R)))
  
     def composition(self, R2):
-        res = set()
-        for t1 in self.R:
-            for t2 in R2.R:
-                if t1[1] == t2[0]: res.add((t1[0], t2[1]))
-        self.R = res
+        return Relation(self.M, R=set((t1[0], t2[1]) for t1 in self.R for t2 in R2.R if t1[1] == t2[0]))
 
     def is_reflexive(self):
-        return 0 == len(filter(lambda m : True if (m, m) not in self.R else False, self.M))
+        return set() == set(filter(lambda m : True if not self.contains(m, m) else False, self.M))
 
     def is_symetric(self):
-        return 0 == len(filter(lambda t : True if (t[1], t[0]) not in self.R else False, self.R))
+        return set() == set(filter(lambda t : True if not self.contains(t[1], t[0]) else False, self.R))
 
     def is_transitive(self):
         for t1 in self.R:
             for t2 in self.R:
-                if t1[1] == t2[0] and (t1[0], t2[1]) not in self.R: return False
+                if t1[1] == t2[0] and not self.contains(t1[0], t2[1]): return False
         return True
+
+    def reflexive_transitive_closure(self):
+        closure = self.R.union(set(map(lambda m : (m, m), self.M)))
+        while True:
+            closure_until_now = closure | set((x,w) for x,y in closure for q,w in closure if q == y)
+            if closure_until_now == closure: break
+            closure = closure_until_now
+        return Relation(self.M, closure_until_now)
