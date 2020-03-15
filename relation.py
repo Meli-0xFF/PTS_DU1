30a8fc7a411aaf8cca007d5181e169354b3c8fdd
Meli-0xFF <martin.melichercik@gmail.com>
2020-03-15 20:38:36 +0100

    Refactoring aftter review, started using psets.
    Signed-off-by: Meli-0xFF <martin.melichercik@gmail.com>

--- relation.py
+++ relation.py
@@ -1,7 +1,9 @@
-class Relation:
-    def __init__(self, M, R=set()):
-        self.R = R
-        self.M = M
+from pyrsistent import pset, PClass, field, PSet
+
+class Relation(PClass):
+
+    M = field(type=PSet, mandatory=True)
+    R = field(type=PSet, initial=pset())
 
     def __iter__(self):
         return self.R.__iter__()
@@ -13,42 +15,42 @@ class Relation:
         return (x,y) in self.R
 
     def add(self, x, y):
-        return Relation(self.M, R=self.R | {(x,y)})
+        return Relation(M=self.M, R=self.R | pset((x,y)))
 
     def remove(self, x, y):
-        return Relation(self.M, R=self.R - {(x,y)})
+        return Relation(M=self.M, R=self.R - pset((x,y)))
 
     def union(self, R2):
-        return Relation(self.M, R=self.R | R2.R)
+        return Relation(M=self.M, R=self.R | R2.R)
 
     def intersection(self, R2):
-        return Relation (self.M, R=self.R & R2.R)
+        return Relation (M=self.M, R=self.R & R2.R)
 
     def subtraction(self, R2):
-        return Relation(self.M, R=self.R - R2.R)
+        return Relation(M=self.M, R=self.R - R2.R)
 
     def inverse(self):
-        return Relation(self.M, R=set(map(lambda t : t[::-1], self.R)))
+        return Relation(M=self.M, R=pset(map(lambda t : t[::-1], self.R)))
  
     def composition(self, R2):
-        return Relation(self.M, R=set((t1[0], t2[1]) for t1 in self.R for t2 in R2.R if t1[1] == t2[0]))
+        return Relation(self.M, R=pset((t1[0], t2[1]) for t1 in self.R for t2 in R2.R if t1[1] == t2[0]))
 
     def is_reflexive(self):
-        return set() == set(filter(lambda m : True if not self.contains(m, m) else False, self.M))
+        return pset() == pset(filter(lambda m : True if not self.contains(m, m) else False, self.M))
 
     def is_symetric(self):
-        return set() == set(filter(lambda t : True if not self.contains(t[1], t[0]) else False, self.R))
+        return pset() == pset(filter(lambda t : True if not self.contains(t[1], t[0]) else False, self.R))
 
     def is_transitive(self):
-        return set() == set((t1, t2) for t1 in self.R for t2 in self.R if t1[1] == t2[0] and not self.contains(t1[0], t2[1]))
+        return pset() == pset((t1, t2) for t1 in self.R for t2 in self.R if t1[1] == t2[0] and not self.contains(t1[0], t2[1]))
 
     def reflexive_transitive_closure(self):
-        closure = self.R.union(set(map(lambda m : (m, m), self.M)))
+        closure = self.R | (pset(map(lambda m : (m, m), self.M)))
         while True:
-            closure_until_now = closure | set((x,w) for x,y in closure for q,w in closure if q == y)
+            closure_until_now = closure | pset((x,w) for x,y in closure for q,w in closure if q == y)
             if closure_until_now == closure: break
             closure = closure_until_now
-        return Relation(self.M, closure_until_now)
+        return Relation(M=self.M, R=closure_until_now)
 
 def get_relation_class(M):
-    return Relation(M)
+    return Relation(M=M)
